8490d887b8fcb0345c66bc9060fdef5508907d95
mydatastorage <42890936+mydatastorage@users.noreply.github.com>
2022-08-29 16:53:37 +0530

    Updates

---
+++ precache-manifest.47079aa5aa4eb76c061406597b4305d1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b123cb8efcde9213654b70dec63242fc",
+    "url": "/myreactproject/index.html"
+  },
+  {
+    "revision": "838a1d7a5a2dd3818852",
+    "url": "/myreactproject/static/css/2.529c2cdb.chunk.css"
+  },
+  {
+    "revision": "a571b47b7d5d12842172",
+    "url": "/myreactproject/static/css/main.7aaae87b.chunk.css"
+  },
+  {
+    "revision": "838a1d7a5a2dd3818852",
+    "url": "/myreactproject/static/js/2.95532599.chunk.js"
+  },
+  {
+    "revision": "a571b47b7d5d12842172",
+    "url": "/myreactproject/static/js/main.46d50c48.chunk.js"
+  },
+  {
+    "revision": "5575fd51ee7588cac316",
+    "url": "/myreactproject/static/js/runtime-main.1029ed80.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/myreactproject/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/myreactproject/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/myreactproject/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/myreactproject/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/myreactproject/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/myreactproject/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/myreactproject/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/myreactproject/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/myreactproject/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/myreactproject/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/myreactproject/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "cc3fff7e1fb8fcdf7ec881e0a394ee72",
+    "url": "/myreactproject/static/media/logo.cc3fff7e.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/myreactproject/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/myreactproject/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/myreactproject/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/myreactproject/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/myreactproject/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
